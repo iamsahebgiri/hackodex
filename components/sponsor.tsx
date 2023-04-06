64992273a2f79dd23f5b4aaade1bd8eaaeacba05
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-04-06 20:52:38 +0530

    Fixed! (#8)
    * Responsive Design
    
    * Minor Changes
    
    * Updated
    
    * Added Sponors logo
    
    * update1
    
    * update2
    
    ---------
    
    Co-authored-by: RiturajGupta21 <rituraj.iter@gmail.com>

--- components/sponsor.tsx
+++ components/sponsor.tsx
@@ -1,11 +1,24 @@
 import { useState, useEffect } from "react";
-import { SponsorType, clubs, community, sponsorevent, communityevent, clubsEvents } from "../seed";
+import { SponsorsType, CommunityType, sponsor, clubs, community, sponsorevent, communityevent, clubsEvents } from "../seed";
 
 const Carousel = () => {
   const [page, setPage] = useState(0);
 
   return (
     <div className="mt-20 md:mt-4">
+
+    <h1 className="mb-3 text-center text-2xl font-semibold text-[#e3c598]">
+          Sponsors
+    </h1>
+    <div className="flex flex-wrap mx-auto justify-center rounded-md w-full overflow-hidden mb-5">
+          {sponsor.map((logo) => (
+            <div key={logo.id} className="rounded-lg md:mx-6 mx-3 my-1 p-2  backdrop-blur-lg backdrop-filter overflow-hidden justify-center mt-5">
+              <a href={logo.url}>
+                <img src={logo.logo} alt={logo.name} className="h-10 max-w-xs" />
+              </a>
+            </div>
+          ))}
+    </div>
 
   <h1 className="mb-3 text-center text-2xl font-semibold text-[#e3c598]">
           Community Partners
@@ -21,7 +34,7 @@ const Carousel = () => {
     </div>
 
     <h1 className="mb-3 mt-24 md:mt-12 text-center text-2xl font-semibold text-[#e3c598]">
-          In Collaboration with
+          Clubs in Collaboration
     </h1>
     <div className="flex flex-wrap mx-auto justify-center rounded-md w-full overflow-hidden mb-5">
           {clubs.map((logo) => (
@@ -41,32 +54,44 @@ const Carousel = () => {
       
       <h3 className="font-semibold text-[#e3c598]">Sponsor Event</h3>
       <div className="flex flex-wrap mx-auto justify-center rounded-md w-full overflow-hidden mb-5">
-      {sponsorevent.map((data) => (
-            <div key={data.id} className="rounded-lg mx-3 my-1 p-2  backdrop-blur-lg backdrop-filter overflow-hidden justify-center mt-5">
+      {/* {sponsorevent.map((data) => (
+            <div key={data.id} className="rounded-lg mx-3 my-1 p-2 backdrop-blur-lg backdrop-filter overflow-hidden justify-center mt-5">
               <a href={data.url}>
-                <h3 className="rounded-md bg-sky-500/[.09] hover:bg-cyan-600 text-white px-2">{data.name}</h3>
+                <h1 className="rounded-md bg-sky-500/[.09] hover:bg-cyan-600 text-white px-2">
+                  {data.name}
+                  </h1>
               </a>
             </div>
-          ))}
+          ))} */}
+          <h1 className="animate-pulse text-2xl ml-4 my-2 font-semibold text-[#d58345]">Coming Soon...</h1>
       </div>
 
       <h3 className="font-semibold text-[#e3c598]">Community Event</h3>
       <div className="flex flex-wrap mx-auto justify-center rounded-md w-full overflow-hidden mb-5">
-        {communityevent.map((data) => (
-            <div key={data.id} className="rounded-lg mx-3 my-1 p-2  backdrop-blur-lg backdrop-filter overflow-hidden justify-center mt-5">
+        {/* {communityevent.map((data) => (
+            <div key={data.id} className="rounded-lg mx-3 my-1 p-2 backdrop-blur-lg backdrop-filter overflow-hidden justify-center mt-5">
               <a href={data.url}>
-                <h3 className="rounded-md bg-sky-500/[.09] hover:bg-cyan-600 text-white px-2">{data.name}</h3>
+                <h1 className="rounded-md bg-sky-500/[.09] hover:bg-cyan-600 text-white px-2">
+                  {data.name}
+                  </h1>
               </a>
             </div>
-          ))}
+          ))} */}
+          <h1 className="animate-pulse text-2xl ml-4 my-2 font-semibold text-[#d58345]">Coming Soon...</h1>
       </div>
           
       <h3 className="font-semibold text-[#e3c598]">Clubs Events</h3>
       <div className="flex flex-wrap mx-auto justify-center rounded-md w-full overflow-hidden mb-5">
           {clubsEvents.map((data) => (
-            <div key={data.id} className="rounded-lg mx-3 my-1 p-2  backdrop-blur-lg backdrop-filter overflow-hidden justify-center mt-5">
-              <a href={data.url}>
-                <h3 className="rounded-md bg-sky-500/[.09] hover:bg-cyan-600 text-white px-2">{data.name}</h3>
+            <div key={data.id} className="rounded-lg mx-3 my-1 p-2 backdrop-blur-lg backdrop-filter overflow-hidden justify-center mt-5">
+              <a href={data.url} className="flex">
+                <div className="my-auto mx-3">
+                <h1 className="rounded-md text-[#d58345] text-start text-3xl font-semibold">
+                  {data.name} 
+                </h1>
+                <p className="text-[#e3c598] md:text-left text-sm ml-1">{data.date}</p>
+                <p className="text-[#e3c598] md:text-left text-sm ml-1">{data.venue}</p>
+                </div>
               </a>
             </div>
           ))}
